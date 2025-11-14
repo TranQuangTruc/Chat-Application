0ab51d8ce641f48990bfc4a1ed136012e3fb0ae3
TranDaiPhu185 <trandaiphu185@gmail.com>
2025-11-14 17:00:15 +0700

    Update Register

--- chap_app_ui/app.py
+++ chap_app_ui/app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def login():
-    return render_template("login.html")
+    return render_template("login.html", title="Đăng nhập")
+
+@app.route("/register")
+def register():
+    return render_template("register.html", title="Đăng ký")
 
 if __name__ == "__main__":
     app.run(debug=True)
