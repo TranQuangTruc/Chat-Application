ca7293c8922af21a2c985775bb1ece21da8a435c
TranDaiPhu185 <trandaiphu185@gmail.com>
2025-11-17 17:23:58 +0700

    profile - settings

--- chap_app_ui/app.py
+++ chap_app_ui/app.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def login():
-    return render_template("login.html", title="Đăng nhập")
-
-@app.route("/register")
-def register():
-    return render_template("register.html", title="Đăng ký")
-
-@app.route("/home")
 def home():
-    return render_template("home.html", title="Chat List")
+    return render_template("home.html")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
+@app.route("/settings")
+def settings():
+    return render_template("settings.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
